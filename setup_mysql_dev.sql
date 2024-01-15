9b3562a0cb0c7831f6f0efed5785a6548755326a
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-15 15:11:11 -0800

    MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Create or use hbnb_dev_db database
+CREATE DATABASE IF NOT EXISTS 	HBNB_DEV_DB;
+
+-- Create or use hbnb_dev user with the specified password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privileges on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
