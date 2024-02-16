c8f6df675ed4f13139c80ce8761db5e5a74e2883
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-02-16 12:05:09 +0000

    C is cool

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This scripts starts a Flask web application"""
+
+
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	"""Route that displays 'Hello HBNB!'"""
+	return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	"""Route that displays 'HBNB'"""
+	return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+	"""Route that displays 'C' followed by value of the text variable"""
+	return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
