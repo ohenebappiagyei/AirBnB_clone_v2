794e14c4f3aebe1868e4161a829e75d06e9f12ed
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-02-18 07:23:40 -0800

    go on

--- models/state.py
+++ models/state.py
@@ -5,8 +5,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
-from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
@@ -14,22 +12,16 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
 
     @property
     def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+        """Getter method to return the list of City objects linked to the current State"""
+        all_cities = models.storage.all(City)
+        return [city for city in all_cities.values() if city.state_id == self.id]
+
